c6b1c641f301fcc67893904dca9e77af2cc3d867
Robert Spitzenpfeil <robert@linuxbox.site>
2012-04-03 14:23:28 +0200

    Updated 'scale.sh' to accept a layer number for the logo.

--- OSHW_logo/source/scale.pl
+++ OSHW_logo/source/scale.pl
@@ -2,13 +2,14 @@
 
 sub usage();
 
-if(scalar(@ARGV) < 3) {
+if(scalar(@ARGV) < 4) {
 	usage();
 }
 
 my $outfile = $ARGV[1];
-my $size = $ARGV[2]; # in 'mm' or in 'in'
+my $size = $ARGV[3]; # in 'mm' or in 'in'
 my $factor;
+my $layer = $ARGV[2];
 
 open(FILE, '<', $ARGV[0]) || die("cannot open: $!");
 my @infiledata = <FILE>;
@@ -31,7 +32,10 @@ foreach $line (@infiledata) {
 		print FILE $1," ",int($2*$factor)," ",int($3*$factor),"\n";
 	} elsif($tmp =~ m/^T(\d{1})\ 0\ (-{0,1}\d*)\ (-{0,1}\d*)\ (-{0,1}\d*)\ (-{0,1}\d*)\ (-{0,1}\d*)(.*)$/) {
 		print FILE "T$1 0 ",int($2*$factor)," ",int($3*$factor)," ",int($4*$factor)," ",int($5*$factor)," ",int($6*$factor),$7,"\n";
-	} else {
+	} elsif($tmp =~ m/^(.*DP)\ (-{0,1}\d*)\ (-{0,1}\d*)\ (-{0,1}\d*)\ (-{0,1}\d*)\ (-{0,1}\d*)\ (-{0,1}\d*)\ (-{0,1}\d*)(.*)$/) {
+		print FILE "$1 $2 $3 $4 $5 $6 $7 $layer\n";
+	}
+	else {
 		print FILE $line;
 	}
 }
@@ -39,7 +43,13 @@ foreach $line (@infiledata) {
 close(FILE);
 
 sub usage() {
-	print "\nusage: scale.pl <infile.emp> <outfile.emp> <size: e.g. 5.00mm or 0.25in>\n\n";
+	print "\nusage: scale.pl <infile.emp> <outfile.emp> <layer number> <size: e.g. 5.00mm or 0.25in>\n\n";
+	print "KiCad layers:\n";
+	print "-------------\n\n";
+	print "Top copper:         0\n";
+	print "Top silkscreen:    21\n\n";
+	print "To move the logo to bottom copper/silkscreen\n";
+	print "move the curser over it and press F for flip layer\n\n";
 	exit;
 }
 
